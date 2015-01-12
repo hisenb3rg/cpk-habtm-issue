547b7d42ce46826b4975c6be21758598e1fd536c
Uros Jurglic <jurglic@gmail.com>
2015-01-12 10:31:11 +0100

    Fresh Rails 4.0 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Habtm2::Application.config.secret_key_base = 'c82eab21dfad3580dcb66fc660c26bbe2bca81ccdef122451235beda016e6fdfd617c386b98efef014d0087b460e6165bb4afd718450bf723b1ef899f926c4d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Habtm2::Application.config.session_store :cookie_store, key: '_habtm2_session'
